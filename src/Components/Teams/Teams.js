2779295c7e2122be6b15dafe10d565e0005e779f
DARSHAN JAIN <84950175+darshan4903@users.noreply.github.com>
2021-08-29 21:05:03 +0530

    Co-authored-by: Mohd Zubair <Mohdzubair3056@users.noreply.github.com>

--- src/Components/Teams/Teams.js
+++ src/Components/Teams/Teams.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import './Teams.css'
+import img from './pawan.png'
 
 const Teams = () => {
     return (
-        <div>
+        <>
+        <div class = 'space'>
             <h1>Teams</h1>
         </div>
+             <div class="container">
+               <div class="card">
+               <div class="circle">
+                <h2 class='text'>Pawan</h2>
+                <h3 class='text'>(Captain)</h3>
+                <h3 class='text'>Electronics and Communication 3rd year</h3>
+               </div>
+                 <div class="content">
+                    <p>  </p>
+                    <div class="photos">
+                    <div class="sketch">
+                    <p><img src={img} class ='hover-img' alt="img"/></p>
+                 
+               </div>
+             </div>
+            
+           </div>
+         </div>
+         </div>
+         </>
+
     )
 }
 
