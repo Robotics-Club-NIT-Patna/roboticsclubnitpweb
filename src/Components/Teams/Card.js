bfcac2d1bbb731dfd7ad8c40aaadd00c303c67da
pratik8696 <mepratikanand@gmail.com>
2021-08-30 09:22:18 +0530

    added teams component

---
+++ src/Components/Teams/Card.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import "./Card.css"
+
+function Card() {
+    return (
+        <div>
+
+            <section class="team-one" id="team">
+                <div class ="container">
+                <div class ="title text-center">
+                
+                </div>
+                <div class ="row">
+                <div class ="col-lg-3 col-md-6 col-sm-12">
+                <div class ="single">
+                <div class ="circle"></div>
+                <div class ="inner">
+                <h3>Brooke John</h3>
+                <p>Web Designer</p>
+                <div class ="image">
+                <img src="/team1.png" alt="" />
+                </div>
+                <div class ="social">
+                <a href="#"><i class ="fa fa-facebook"></i></a>
+                <a href="#"><i class ="fa fa-twitter"></i></a>
+                <a href="#"><i class ="fa fa-instagram"></i></a>
+                </div>
+                </div>
+                </div>
+                </div>
+                <div class ="col-lg-3 col-md-6 col-sm-12">
+                <div class ="single">
+                <div class ="circle"></div>
+                <div class ="inner">
+                <h3>Kubos Ted</h3>
+                <p>Designer</p>
+                <div class ="image">
+                <img src="/team2.png" alt="" />
+                </div>
+                <div class ="social">
+                <a href="#"><i class ="fa fa-facebook"></i></a>
+                <a href="#"><i class ="fa fa-twitter"></i></a>
+                <a href="#"><i class ="fa fa-instagram"></i></a>
+                </div>
+                </div>
+                </div>
+                </div>
+                <div class ="col-lg-3 col-md-6 col-sm-12">
+                <div class ="single">
+                <div class ="circle"></div>
+                <div class ="inner">
+                <h3>Milky Dos</h3>
+                <p>Developer</p>
+                <div class ="image">
+                <img src="/team3.png" alt="" />
+                </div>
+                <div class ="social">
+                <a href="#"><i class ="fa fa-facebook-square"></i></a>
+                <a href="#"><i class ="fa fa-twitter"></i></a>
+                <a href="#"><i class ="fa fa-instagram"></i></a>
+                </div>
+                </div>
+                </div>
+                </div>
+                <div class ="col-lg-3 col-md-6 col-sm-12">
+                <div class ="single">
+                <div class ="circle"></div>
+                <div class ="inner">
+                <h3>Chen Big</h3>
+                <p>App Designer</p>
+                <div class ="image" >
+                <img src="/team%204.png" alt="" />
+                </div>
+                <div class ="social">
+                <a href="#"><i class ="fa fa-facebook-square"></i></a>
+                <a href="#"><i class ="fa fa-twitter"></i></a>
+                <a href="#"><i class ="fa fa-instagram"></i></a>
+                </div>
+                </div>
+
+                </div>
+                </div>
+                </div>
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default Card
