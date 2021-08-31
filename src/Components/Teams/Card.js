c96db9880535c9405b7103f6010eee28d7dff956
Mohd Zubair <mohdzubair3056@gmail.com>
2021-08-31 16:30:35 +0530

    team page completed

--- src/Components/Teams/Card.js
+++ src/Components/Teams/Card.js
@@ -3,87 +3,20 @@ import "./Card.css"
 
 function Card() {
     return (
-        <div>
-
-            <section class="team-one" id="team">
-                <div class ="container">
-                <div class ="title text-center">
-                
+        <div className ="single">
+            <div className ="circle"></div>
+            <div className ="inner">
+                <h3 id="memberName">Brooke John</h3>
+                <p id="memberDesignation">Web Designer</p>
+                <div className ="image">
+                    <img src="/team1.png" alt="" />
                 </div>
-                <div class ="row">
-                <div class ="col-lg-3 col-md-6 col-sm-12">
-                <div class ="single">
-                <div class ="circle"></div>
-                <div class ="inner">
-                <h3>Brooke John</h3>
-                <p>Web Designer</p>
-                <div class ="image">
-                <img src="/team1.png" alt="" />
+                <div className ="social">
+                    <a id="socialLinks" href="#"><i className ="fa fa-facebook"></i></a>
+                    <a id="socialLinks" href="#"><i className ="fa fa-twitter"></i></a>
+                    <a id="socialLinks" href="#"><i className ="fa fa-instagram"></i></a>
                 </div>
-                <div class ="social">
-                <a href="#"><i class ="fa fa-facebook"></i></a>
-                <a href="#"><i class ="fa fa-twitter"></i></a>
-                <a href="#"><i class ="fa fa-instagram"></i></a>
-                </div>
-                </div>
-                </div>
-                </div>
-                <div class ="col-lg-3 col-md-6 col-sm-12">
-                <div class ="single">
-                <div class ="circle"></div>
-                <div class ="inner">
-                <h3>Kubos Ted</h3>
-                <p>Designer</p>
-                <div class ="image">
-                <img src="/team2.png" alt="" />
-                </div>
-                <div class ="social">
-                <a href="#"><i class ="fa fa-facebook"></i></a>
-                <a href="#"><i class ="fa fa-twitter"></i></a>
-                <a href="#"><i class ="fa fa-instagram"></i></a>
-                </div>
-                </div>
-                </div>
-                </div>
-                <div class ="col-lg-3 col-md-6 col-sm-12">
-                <div class ="single">
-                <div class ="circle"></div>
-                <div class ="inner">
-                <h3>Milky Dos</h3>
-                <p>Developer</p>
-                <div class ="image">
-                <img src="/team3.png" alt="" />
-                </div>
-                <div class ="social">
-                <a href="#"><i class ="fa fa-facebook-square"></i></a>
-                <a href="#"><i class ="fa fa-twitter"></i></a>
-                <a href="#"><i class ="fa fa-instagram"></i></a>
-                </div>
-                </div>
-                </div>
-                </div>
-                <div class ="col-lg-3 col-md-6 col-sm-12">
-                <div class ="single">
-                <div class ="circle"></div>
-                <div class ="inner">
-                <h3>Chen Big</h3>
-                <p>App Designer</p>
-                <div class ="image" >
-                <img src="/team%204.png" alt="" />
-                </div>
-                <div class ="social">
-                <a href="#"><i class ="fa fa-facebook-square"></i></a>
-                <a href="#"><i class ="fa fa-twitter"></i></a>
-                <a href="#"><i class ="fa fa-instagram"></i></a>
-                </div>
-                </div>
-
-                </div>
-                </div>
-                </div>
-                </div>
-            </section>
-
+            </div>
         </div>
     )
 }
