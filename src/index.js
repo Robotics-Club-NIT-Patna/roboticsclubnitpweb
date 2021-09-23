511bb36aa68a9c850df08c4e59e7632e28ad1d4e
Mohd Zubair <mohdzubair3056@gmail.com>
2021-09-23 14:15:37 +0530

    land on top of page while routing resolved

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import ScrollTop from './ScrollTop';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollTop/>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
